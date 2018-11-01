95891a7d3d11989c6b647f97910f87d8a7491528
ageuribeiro <16623986+ageuribeiro@users.noreply.github.com>
2018-10-31 21:27:09 -0300

    atualizacao -  implementação dos botoes na tela de acesso

--- index.php
+++ index.php
@@ -14,8 +14,33 @@
     <div>
         <form action="#" method="POST" id="#form_login">
             <fieldset>
-                <legend></legend>
-                
+                <legend>Acesso Restrito</legend>
+                    <table>
+                        <tr>
+                            <td>
+                                <label for="username">username: </label>
+                            </td>
+                            <td>
+                                <input type="text" name="username" id="username">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <label for="password">password: </label>
+                            </td>
+                            <td>
+                                <input type="password" name="password" id="password">   
+                            </td>
+                        </tr>
+                        <tr>
+                        <td>
+                            <input type="submit" value="Acessar" name="btn_aces">
+                        </td>
+                        <td>
+                            <input type="submit" value="Cadastrar" name="btn_cad">
+                        </td>
+                        </tr>
+                    </table>
             </fieldset>
         </form>
     </div>
