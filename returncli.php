22e2ae619f50b302488c792b5cb0a752324392f9
ageuribeiro <16623986+ageuribeiro@users.noreply.github.com>
2018-10-31 21:45:48 -0300

    inclusao de arquivo

---
+++ returncli.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Satisfação do Cliente - ASTI</title>
+</head>
+<body>
+    <div>
+        <header>
+            <h3>
+                Como você avalia o nosso serviço hoje?
+            </h3>
+        </header>
+        <table>
+            <tr><td><button>Ótimo</button></td></tr>
+            <tr><td><button>Bom</button></td></tr>
+            <tr><td><button>Regular</button></td></tr>
+            <tr><td><button>Ruim</button></td></tr>
+        </table>
+    </div>
+</body>
+</html>
