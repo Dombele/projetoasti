cf9eeb30cb76d94f9fb66862912e029b889fb179
ageuribeiro <16623986+ageuribeiro@users.noreply.github.com>
2018-10-31 21:04:33 -0300

    atualização da tela de login

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>login - ASTI</title>
+</head>
+<body>
+    <div>
+        <form action="#" method="POST" id="#form_login">
+            <fieldset>
+                <legend></legend>
+                
+            </fieldset>
+        </form>
+    </div>
+</body>
+</html>
